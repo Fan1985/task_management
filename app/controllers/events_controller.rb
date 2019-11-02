b32b34a3c23c1d3447cd4e97d6e0592a3b4edca5
fahn <wfliu.1005@gmail.com>
2019-11-02 11:11:09 +0800

    build new.html.erb for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,23 @@ class EventsController < ApplicationController
     
   end
   def new
+    @event = Event.new #產生 @event 讓 new 表單可以接資料
   end
   def create
+    @event = Event.new(clean_params) 
+    if @event.save
+      redirect_to task_path, notice: '任務新建完成! '
+    else
+      render :new
+    end
   end
   def show
   end
   def destroy
   end
+
+  private
+    def clean_params
+      params.require(:event).permit(:topic, :content, :priority, :status, :start_from, :end_at)
+    end
 end
