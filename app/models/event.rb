2b0ed07949b54a7f55f8fa913f2fdfcaef9a0c61
fahn <wfliu.1005@gmail.com>
2019-11-04 11:28:52 +0800

    done with event creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ApplicationRecord
-  belongs_to :user
-end
+  # belongs_to :user
+#   validate :confirm_status
+  
+#   enum status: {'未開始': '0', '進行中': '1', '已完成': '2'}
+
+#   def confirm_status
+#     if Time.now < self.start_from
+#       self.status == 1
+#     else
+#       self.status == 2
+#     end
+#   end
+# end
